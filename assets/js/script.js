8d3f923e0d6510a393d7d7c9a4638111ba2dcf28
Alessandro Mendes <165946341+AlessandroMendesS@users.noreply.github.com>
2025-02-07 11:10:30 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,3 @@ var options = {
 var typed = new Typed(".typing", options);
 
 
-document.addEventListener('mousemove', (e) => {
-    const cursor = document.querySelector('.cursor');
-    cursor.style.top = e.clientY + 'px';
-    cursor.style.left = e.clientX + 'px';
-});
-
