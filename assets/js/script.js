064a48a7aab09f0b602cab58eeb79c529b0fd86f
Alessandro Mendes <165946341+AlessandroMendesS@users.noreply.github.com>
2025-02-07 11:16:51 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
 var options = {
   strings: ["Programador", "Designer", "Alessandro Mendes"],
-  typeSpeed: 50,
-  backSpeed: 25,
-  loop: true
+  typeSpeed: 50,     
+  backSpeed: 25,      
+  loop: true         
 };
 
 var typed = new Typed(".typing", options);
-
 
